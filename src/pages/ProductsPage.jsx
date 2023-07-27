e059fdf7d9b2727b26290e2e4d24a9c53ceb33ac
loqman1235 <tonyvito12@gmail.com>
2023-07-27 08:37:04 +0100

    commit

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -91,7 +91,7 @@ const ProductsPage = () => {
                   <th>
                     <div className="avatar">
                       <div className="w-10 rounded-sm">
-                        <img src={product?.images[0].url} />
+                        <img src={product?.images[0]?.url} />
                       </div>
                     </div>
                   </th>
